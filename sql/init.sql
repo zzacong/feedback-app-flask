16101ff6a9418203c419ccba3de15d675903f69c
Zhi Zao Ong <Zhi.Zao.Ong@ibm.com>
2021-03-04 21:48:43 +1100

    feat: update sql init

--- sql/init.sql
+++ sql/init.sql
@@ -1,3 +1,15 @@
-DROP DATABASE IF EXISTS `lexus`;
+DROP DATABASE IF EXISTS "lexus";
 
-CREATE DATABASE `lexus`;
+CREATE DATABASE "lexus";
+
+\connect "lexus";
+
+DROP TABLE IF EXISTS "feedback";
+
+CREATE TABLE "feedback" (
+  id SERIAL PRIMARY KEY,
+  customer VARCHAR(200) UNIQUE,
+  dealer VARCHAR(200),
+  rating INT,
+  comments TEXT
+);
