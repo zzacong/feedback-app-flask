4f32df56a8e7f23781b44ef6a61b798f994a7111
zzacong <ongzz100@gmail.com>
2021-06-11 16:05:21 +1000

    feat(env): use dotenv

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
+import os
 from flask import Flask, render_template, request, logging
 from flask_sqlalchemy import SQLAlchemy
 from send_mail import send_mail
-
 
 # ! Init App
 app = Flask(__name__)
 
 # ! App Configs
-# ENV = 'development'
-ENV = 'production'
-if ENV == 'development':
-    app.debug = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/lexus'
-else:
-    app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://epqqeiffpvsesl:7abc6ff3f6f3471063f370160dd738ed7f9b5e8a7e2bdcbaaab137f1884d3124@ec2-3-230-247-88.compute-1.amazonaws.com:5432/dco7io1q7dfpp7'
+if app.env == 'development':
+  print('env', app.env)
+  import dotenv
+  dotenv.load_dotenv(override=True)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # ! Init db
@@ -24,45 +21,45 @@ db = SQLAlchemy(app)
 
 # ! Model
 class Feedback(db.Model):
-    __tablename__ = 'feedback'
-    id = db.Column(db.Integer, primary_key=True)
-    customer = db.Column(db.String(200), unique=True)
-    dealer = db.Column(db.String(200))
-    rating = db.Column(db.Integer)
-    comments = db.Column(db.Text())
+  __tablename__ = 'feedback'
+  id = db.Column(db.Integer, primary_key=True)
+  customer = db.Column(db.String(200), unique=True)
+  dealer = db.Column(db.String(200))
+  rating = db.Column(db.Integer)
+  comments = db.Column(db.Text())
 
-    def __init__(self, customer, dealer, rating, comments):
-        self.customer = customer
-        self.dealer = dealer
-        self.rating = rating
-        self.comments = comments
+  def __init__(self, customer, dealer, rating, comments):
+    self.customer = customer
+    self.dealer = dealer
+    self.rating = rating
+    self.comments = comments
 
 
 # ! Index
 @app.route('/')
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
 
 
 # ! Submit
 @app.route('/submit', methods=['POST'])
 def submit():
-    if request.method == 'POST':
-        customer = request.form['customer']
-        dealer = request.form['dealer']
-        rating = request.form['rating']
-        comments = request.form['comments']
-        # app.logger.info(f'{customer}, {dealer}, {rating}, {comments}')
-        if customer == '' or dealer == '':
-            return render_template('index.html', msg='Please enter required fields.')
-        if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
-            data = Feedback(customer, dealer, rating, comments)
-            db.session.add(data)
-            db.session.commit()
-            send_mail(customer, dealer, rating, comments)
-            return render_template('success.html')
-        return render_template('index.html', msg='You can only submit one feedback.')
+  if request.method == 'POST':
+    customer = request.form['customer']
+    dealer = request.form['dealer']
+    rating = request.form['rating']
+    comments = request.form['comments']
+    # app.logger.info(f'{customer}, {dealer}, {rating}, {comments}')
+    if customer == '' or dealer == '':
+      return render_template('index.html', msg='Please enter required fields.')
+    if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
+      data = Feedback(customer, dealer, rating, comments)
+      db.session.add(data)
+      db.session.commit()
+      send_mail(customer, dealer, rating, comments)
+      return render_template('success.html')
+    return render_template('index.html', msg='You can only submit one feedback.')
 
 
 if __name__ == '__main__':
-    app.run()
+  app.run()
