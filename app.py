7b5da9c10423cca5227c393549d55ce1a1b7d679
Zac Ong <zac.ong@ibm.com>
2022-11-24 19:54:24 +1100

    database_url

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ if app.env == 'development':
   import dotenv
   dotenv.load_dotenv(override=True)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('POSTGRES_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # ! Init db
